8321e8a015d5306c4f7ad1cade9adb045cd8a1af
zkonge <zkonge@outlook.com>
2021-03-31 01:02:33 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # meilisearch-windows-patch
 Prevent meilisearch LMDB to eat all disk space in Windows
+
+Thanks to https://github.com/Flakebi/lmdb-rs
---
+++ build.py
@@ -0,0 +1,25 @@
+import subprocess
+
+PATCH = """
+[patch.crates-io]
+lmdb-rkv-sys = { git = "https://github.com/ForensicCat/lmdb-rs" }
+"""
+
+
+class BuildError(Exception):
+    pass
+
+
+def run(cmd: str) -> int:
+    p = subprocess.Popen(cmd)
+    r = p.wait()
+    if r:
+        raise BuildError(f'A Error raised when executing "{cmd}"')
+
+
+run("git clone https://github.com/meilisearch/MeiliSearch.git --depth=1 .")
+
+with open("Cargo.toml", "a") as f:
+    f.write(PATCH)
+
+run("cargo build --release")
